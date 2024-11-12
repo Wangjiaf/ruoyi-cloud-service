3682e182f659bc23f51160cd1d7242843a31d85e
wangjiafu <1476391480@qq.com>
2024-11-12 11:33:49 +0800

    初始化

---
+++ bin/move.bat
@@ -0,0 +1,31 @@
+@echo off
+echo.
+echo 批量复制jar包到本路径
+echo.
+echo.
+echo.
+echo  开始复制 ..\ruoyi-modules\ruoyi-system\target\ruoyi-modules-system.jar 执行结果：
+copy ..\ruoyi-modules\ruoyi-system\target\ruoyi-modules-system.jar .\ruoyi-modules-system.jar /y
+echo.
+echo  开始复制 ..\ruoyi-modules\ruoyi-job\target\ruoyi-modules-job.jar 执行结果：
+copy ..\ruoyi-modules\ruoyi-job\target\ruoyi-modules-job.jar .\ruoyi-modules-job.jar /y
+echo.
+echo  开始复制 ..\ruoyi-visual\ruoyi-monitor\target\ruoyi-visual-monitor.jar 执行结果：
+copy ..\ruoyi-visual\ruoyi-monitor\target\ruoyi-visual-monitor.jar .\ruoyi-visual-monitor.jar /y
+echo.
+echo  开始复制 ..\ruoyi-modules\ruoyi-chat\target\ruoyi-modules-chat.jar 执行结果：
+copy ..\ruoyi-modules\ruoyi-chat\target\ruoyi-modules-chat.jar .\ruoyi-modules-chat.jar /y
+echo.
+echo  开始复制 ..\ruoyi-gateway\target\ruoyi-gateway.jar 执行结果：
+copy ..\ruoyi-gateway\target\ruoyi-gateway.jar .\ruoyi-gateway.jar /y
+echo.
+echo  开始复制 ..\ruoyi-auth\target\ruoyi-auth.jar 执行结果：
+copy ..\ruoyi-auth\target\ruoyi-auth.jar .\ruoyi-auth.jar /y
+echo.
+echo  开始复制 ..\ruoyi-modules\ruoyi-file\target\ruoyi-modules-file.jar 执行结果：
+copy ..\ruoyi-modules\ruoyi-file\target\ruoyi-modules-file.jar .\ruoyi-modules-file.jar /y
+echo.
+echo  开始复制 ..\ruoyi-modules\ruoyi-gen\target\ruoyi-modules-gen.jar 执行结果：
+copy ..\ruoyi-modules\ruoyi-gen\target\ruoyi-modules-gen.jar .\ruoyi-modules-gen.jar /y
+echo.
+pause
